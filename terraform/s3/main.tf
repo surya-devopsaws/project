911386cb664a842e857d44259b5dacc1b843883e
umeshqtree <125949602+umeshqtree@users.noreply.github.com>
2024-01-11 20:16:29 +0530

    Update main.tf

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   #secret_key = "update_keys"
 }
 resource "aws_s3_bucket" "demo" {
-     bucket = "qtree-devops-bucket-5"
+     bucket = "qtree-devops-bucket-6"
 }
 output "bucket_arn" {
     value = aws_s3_bucket.demo.arn
