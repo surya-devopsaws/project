4ece5bad8c9e5d1a8e5854ebbebfbce0d9464794
surya-devopsaws <152679179+surya-devopsaws@users.noreply.github.com>
2024-06-12 09:46:12 +0530

    Update main.tf

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
   #access_key = "update_keys"
-  #secret_key = "update_"
+  #secret_key = "update_keys"
 }
 resource "aws_s3_bucket" "demo" {
      bucket = "qtree-devops-bucket-4"
