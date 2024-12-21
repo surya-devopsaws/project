abedbb600eaac0f071e5cd5fa07db1ccc8fd432c
surya-devopsaws <152679179+surya-devopsaws@users.noreply.github.com>
2024-12-21 08:53:23 +0530

    Update main.tf

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
   region = "ap-south-1"  # Set your desired AWS region
-  #access_key = "AKIAUURQQB2JYGELDP7E"
-  #secret_key = "F8S9EqYTLDGYfpf/94kOQY9dzoFIrVmmmbkpcozL"
+  //#access_key = ""
+  //#secret_key = ""
 }
 
 resource "aws_instance" "project1" {
   tags = {
     Name = "project"
   }
-  ami           = "ami-03f4878755434977f"  # Replace with your desired AMI ID
+  //ami           = "ami-03f4878755434977f"  # Replace with your desired AMI ID
   instance_type = "t2.micro"  # Replace with your desired instance type
   key_name      = "qtree"  # Replace with your key pair name
   subnet_id       = "subnet-07dde7003a212c97c"
